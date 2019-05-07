524c8e0d518ef8b7523e78002827a9761152682f
sarrvesh <sarrvesh.ss@gmail.com>
2019-05-07 15:49:51 +0200

    parse command line input using argp

--- src/sieve.c
+++ src/sieve.c
@@ -3,15 +3,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <gmp.h>
+#include <argp.h>
 
-#define NUM_INPUTS 3
-#define VERSION_STR "0.1"
 #define BASE10 10
 
 struct node {
    mpz_t value;
    struct node *next;
 };
+
+/* Global variables for argument parser */
+const char *argp_program_version = "sieve 0.1";
+const char *argp_program_bug_address = "<sarrvesh.ss@gmail.com>";
+static char doc[] = 
+    "Generate prime numbers using various sieving methods.";
 
 /******************************************************************************
 * 
@@ -123,7 +128,7 @@ bool move(struct node **prev, mpz_t N) {
 *  By this function ends, the list should only contain prime numbers.
 *
 ******************************************************************************/
-void doSieve(struct node **Head, struct node **Tail) {
+void doSieveEratoshthenes(struct node **Head, struct node **Tail) {
    mpz_t hopSize, hopSize_1;
    mpz_init(hopSize);
    mpz_init(hopSize_1);
@@ -163,6 +168,64 @@ void doSieve(struct node **Head, struct node **Tail) {
    }
 }
 
+struct arguments {
+    char *limit;
+    char *outFileName;
+    char *method;
+    FILE *fp;
+};
+
+/******************************************************************************
+* 
+* Function to handle command line input
+*
+******************************************************************************/
+static int parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = state->input;   
+    switch(key) {
+        case 'm':
+            arguments->method = arg;
+            break;
+        case 'o':
+            arguments->outFileName = arg;
+            break;
+        case 'l':
+            arguments->limit = arg;
+            break;
+    }
+   return(0);
+}
+
+/******************************************************************************
+* 
+* Validate the parsed command line input
+*
+******************************************************************************/
+void validateCLI(struct arguments *arguments) {
+   if(arguments->limit == NULL) {
+      printf("ERROR: Option -l undefined\n");
+      printf("Try `sieve --help' or `sieve --usage' for more information.\n\n");
+      exit(1);
+   }
+   if(arguments->outFileName == NULL) {
+      printf("INFO: Option -o undefined\n");
+      printf("INFO: Output will be redirected to standard output.\n");
+      arguments->fp = stdout;
+   }
+   else {
+      arguments->fp = fopen(arguments->outFileName, "w+");
+      if(arguments->fp == NULL) {
+         printf("ERROR: Unable to open output file.\n");
+         printf("ERROR: Terminating execution\n");
+         exit(0);
+      }
+   }
+   if(arguments->method == NULL) {
+      printf("INFO: Option -e undefined. Defaulting to method 'e'.\n");
+      arguments->method = "e";
+   }
+}
+
 /******************************************************************************
 * 
 * Main function
@@ -175,39 +238,32 @@ int main(int argc, char *argv[]) {
    struct node *Tail = NULL;
    clock_t start, end;
    float createT, printT, freeT, sieveT;
-   FILE *fp = NULL;
 
-   /* Declaration GMP variables and initialize them */
+   /* Parse the command line input */
+   struct arguments arguments = { 0 };
+   static struct argp_option options[] = {
+    {0, 'l', "limit",  0, "Largest number upto which the code will\nlist prime numbers."},
+    {0, 'o', "output", 0, "Name of the output file."},
+    {0, 'm', "method", 0, "Type of sieving method to generate prime numbers.\nValid options are:\n   e (for Sieve of Eratosthenes)."},
+    {0} // An empty terminating record
+   };
+   struct argp argp = {options, parse_opt, 0, doc};
+   argp_parse(&argp, argc, argv, 0, 0, &arguments);
+   validateCLI(&arguments);
+
+   //printf("DEBUG: Compiled with GNU MP version %d.%d\n",
+   //          __GNU_MP_VERSION, __GNU_MP_VERSION_MINOR);
+   //printf("DEBUG: GNU MP built with %s using flags:\n", __GMP_CC);
+   //printf("   ");
+   //puts(__GMP_CFLAGS);
+   //printf("\n");
+      
+   /* Convert the input limit to a GMP variable and initialize */
    mpz_t upperBound, loopIdx, nPrimes;
    mpz_init(upperBound);
    mpz_init(loopIdx);
    mpz_init(nPrimes);
-
-   printf("\n");
-   printf("Sieve of Eratosthenes v%s\n", VERSION_STR);
-   printf("Written by Sarrvesh S. Sridhar\n\n");
-
-   printf("DEBUG: Compiled with GNU MP version %d.%d\n",
-             __GNU_MP_VERSION, __GNU_MP_VERSION_MINOR);
-   printf("DEBUG: GNU MP built with %s using flags:\n", __GMP_CC);
-   printf("   ");
-   puts(__GMP_CFLAGS);
-   printf("\n");
-
-   /* Parse the command line input */
-   if((argc != NUM_INPUTS)) {
-      printf("ERROR: Invalid command line input. Terminating execution!\n");
-      printf("Usage: %s <limit> <output file name>\n\n", argv[0]);
-      exit(1);
-   }
-      
-   /* Read command line input */
-   mpz_set_str(upperBound, argv[1], BASE10);
-   fp = fopen(argv[2], "w+");
-   if(fp == NULL) {
-      printf("ERROR: Unable to open the specified output file\n\n");
-      exit(1);
-   }
+   mpz_set_str(upperBound, arguments.limit, BASE10);
 
    /* Initialize the list */
    start = clock();
@@ -222,17 +278,17 @@ int main(int argc, char *argv[]) {
    /* Parse the list and remove nodes containing non-prime numbers */
    start = clock();
    printf("INFO: Generating primes... Please wait\n");
-   doSieve(&Head, &Tail);
+   doSieveEratoshthenes(&Head, &Tail);
    end = clock();
    sieveT = (float)(end - start)/CLOCKS_PER_SEC;
 
    /* Print the final list of prime numbers */
    start = clock();
-   printList(&Head, &Tail, &fp, nPrimes);
+   printList(&Head, &Tail, &(arguments.fp), nPrimes);
    printf("INFO: Found ");
    mpz_out_str(NULL, BASE10, nPrimes);
-   printf(" primes less than %s\n", argv[1]);
-   fclose(fp);
+   printf(" primes less than %s\n", arguments.limit);
+   if(arguments.fp != stdout) { fclose(arguments.fp); }
    end = clock();
    printT = (float)(end - start)/CLOCKS_PER_SEC;
 
